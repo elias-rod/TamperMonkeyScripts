0190591133e2a16c9f4f459428d6d73bab0fc706
Elias Rodriguez <elias.rodriguez@southworks.com>
2020-10-13 17:03:56 -0300

    Update SlackUpdates.js

--- SlackUpdates.js
+++ SlackUpdates.js
@@ -1,44 +1,32 @@
 // ==UserScript==
-// @name       Devops Item - Copy updates message for Slack thread
-// @namespace  http://tampermonkey.net/
+// @name       Open preview For EOD
+// @namespace  devops
 // @version    0.3
 // @description  Open preview For EOD
-// @match      https://dev.azure.com/azurecom/*/_workitems/edit/*
-// @copyright  Pablo Biasotti
-// @author     pbiasotti
+// @match      https://dev.azure.com/southworks/hartwell/_workitems/edit/*
+// @copyright  Elias Rodriguez
+// @author     elias-rod
 // @require http://code.jquery.com/jquery-latest.js
+// @require https://gist.githubusercontent.com/elias-rod/445b11f92161823ca8de5ef0fc514e6b/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
 // @grant        GM_setClipboard
 // @grant        GM_addStyle
 // ==/UserScript==
 
-(function() {
-    'use strict';
+waitForKeyElements (".work-item-form-header-controls-container", addButton);
 
-    var highPriorityLevels = ["high", "4 - high", "urgent", "3 - urgent", "hotfix", "0 - hotfix", "next", "1 - next"];
-
-    setTimeout(function() {
-        $('.work-item-form-header-controls-container')
-            .append('<button id="slackButton">Slack updates</button>');
-
-        $('#slackButton').click(function () {
-            // Get the priority level
-            var itemPriorityLevel = $('label').filter( function (i) { return $(this).html().toLowerCase() === 'priority level'}).parent().next().find('input').val();
-
-            // Build the slack message
-            var priorityIcon = ':thinkingpepe:';
-
-            if (highPriorityLevels.includes(itemPriorityLevel.toLowerCase()))
-            {
-                priorityIcon = ':icrhigh:'
-            }
-
-            GM_setClipboard(priorityIcon + " *[Updates]* `" + document.title.replace(" - Boards", "") + "`\n" + window.location, 'text');
-        });
-    }, 2000);
-})();
+function addButton (jNode) {
+    var segments = window.location.pathname.split("/");
+    var id = segments[segments.length-1];
+    if(id == ""){
+        id = segments[segments.length-2];
+    }
+    $('.work-item-form-header-controls-container')
+        .append('<button id="eodButton">For EOD</button>');
+    $('#eodButton').click(function () { window.open("https://sw-eod-generator.azurewebsites.net/preview/" + id + "?preset=hartwell"); });
+}
 
 GM_addStyle ( `
-    #slackButton {
+    #eodButton {
         background: rgb(0, 120, 212);
         color: rgb(255,255,255);
         cursor: pointer;
